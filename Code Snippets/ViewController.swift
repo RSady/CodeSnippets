967cb2c024b2cab9e8363663120cb38a26d60d08
RSady <ryansady@gmail.com>
2018-06-09 10:52:52 -0400

    Test Commit/Push

--- Code Snippets/ViewController.swift
+++ Code Snippets/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //First time commit
+        //First time commit & push
     }
 
 
