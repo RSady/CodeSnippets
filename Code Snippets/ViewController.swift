fb2fff416bea07934bf228fe3ae9dffa196deb1c
RSady <ryansady@gmail.com>
2018-06-09 10:38:21 -0400

    Initial Commit

---
+++ Code Snippets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Code Snippets
+//
+//  Created by Ryan Sady on 6/9/18.
+//  Copyright © 2018 Ryan Sady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
